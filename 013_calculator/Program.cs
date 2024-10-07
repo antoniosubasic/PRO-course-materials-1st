24b09188ea82af8f0024e68ed274dbca35e3150f
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 013_calculator

---
+++ 013_calculator/Program.cs
@@ -0,0 +1,37 @@
+﻿const string askForNumber = "Please enter the {0} number: ";
+const string result = "The result is: ";
+double value;
+
+Console.Write(askForNumber, "first");
+double firstNumber = double.Parse(Console.ReadLine()!);
+
+Console.Write(askForNumber, "second");
+double secondNumber = double.Parse(Console.ReadLine()!);
+
+Console.Write("Please enter the operation [ +  -  *  / ]: ");
+string operation = Console.ReadLine()!;
+
+switch (operation)
+{
+    case "+":
+        value = firstNumber + secondNumber;
+        break;
+
+    case "-":
+        value = firstNumber - secondNumber;
+        break;
+
+    case "*":
+        value = firstNumber * secondNumber;
+        break;
+
+    case "/":
+        value = firstNumber / secondNumber;
+        break;
+
+    default:
+        Console.WriteLine("You must enter an operation");
+        return;
+}
+
+Console.WriteLine("\n" + result + value);
