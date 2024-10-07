9a4c677221da11802ab58a12648f4e75f9904c10
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 006_circle_area_calculator

---
+++ 006_circle_area_calculator/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+
+Console.Write("Enter the radius of your circle: ");
+int radius = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("The area of your circle is: " + Math.PI * Math.Pow(radius, 2));
