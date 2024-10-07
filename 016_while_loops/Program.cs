e41563bc9af431001e46d2a20389644e4e943982
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 016_while_loops

---
+++ 016_while_loops/Program.cs
@@ -0,0 +1,27 @@
+﻿int first = 0;
+int second = 1;
+int last = 0;
+
+// do while
+do
+{
+    Console.Write(first + " ");
+    last = first;
+    first = second;
+    second += last;
+}
+while (first <= 34);
+
+Console.WriteLine();
+
+// while
+first = 0;
+second = 1;
+last = 0;
+while (first <= 34)
+{
+    Console.Write(first + " ");
+    last = first;
+    first = second;
+    second += last;
+}
