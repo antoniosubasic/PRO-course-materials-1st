80f0b956dfea8649b71916678271784b4cc68726
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:32 +0200

    added 053_hello_arrays

---
+++ 053_hello_arrays/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("How many numbers do you want to enter: ");
+var count = int.Parse(Console.ReadLine()!);
+var numbers = new int[count];
+
+for (var i = 0; i < count; i++)
+{
+    Console.Write($"Please enter the {i + 1}. number: ");
+    numbers[i] = int.Parse(Console.ReadLine()!);
+}
+
+Console.WriteLine(numbers);
+foreach (var number in numbers) { Console.WriteLine(number); }
+
+var fibonacci = new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 };
