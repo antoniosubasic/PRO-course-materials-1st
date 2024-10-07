499f7b981cc93c305c19f1c155e467c2a6966dca
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 029_palindrome

---
+++ 029_palindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+int originalNumber = number;
+
+int numberLength = 1;
+while (number % 10 != number) { numberLength++; number /= 10; }
+number = originalNumber;
+
+int numberReversed = 0;
+for (int i = 1; i <= numberLength; i++)
+{
+    numberReversed += (number % 10) * (int)Math.Pow(10, numberLength - i);
+    number /= 10;
+}
+
+Console.Write("The number is");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write(originalNumber == numberReversed ? "" : " not");
+Console.ResetColor();
+
+Console.Write(" a palindrome");
