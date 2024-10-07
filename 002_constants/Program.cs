1fcb372f778e8f17d5197e3b8801516b4c0e22f9
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 002_constants

---
+++ 002_constants/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+const string HOME_ADRESS = "Hans-Schmidinger-Weg 8";
+Console.WriteLine("I live at {0}", HOME_ADRESS);
+
+const int MY_AGE = 14;
+Console.WriteLine("I am {0} years old", MY_AGE);
