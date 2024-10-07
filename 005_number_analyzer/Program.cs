02ba62cc54171c96c682de7225a5b7089ea0fe66
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 005_number_analyzer

---
+++ 005_number_analyzer/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Please enter a number: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int remainder = number % 2;
+bool isEvenNumber = remainder == 0;
+Console.WriteLine("\nIt is " + isEvenNumber + " that the number is even.");
+
+bool isGreaterThanFive = number > 5;
+Console.WriteLine("It is " + isGreaterThanFive + " that the number is greater than 5.");
+
+number++;
+Console.WriteLine("The next number is " + number);
