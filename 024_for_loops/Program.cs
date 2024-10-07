ad41c55b6083dd05cdc8eaf9ae8e4bfa1bf91750
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 024_for_loops

---
+++ 024_for_loops/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.OutputEncoding = System.Text.Encoding.Default;
+
+for (int i = 0; i < 15; i++)
+{
+    for (int j = 0; j < 15; j++)
+    {
+        Console.Write((i * 15 + j) % 2 == 0 ? "😄" : "🍕");
+    }
+    Console.WriteLine();
+}
