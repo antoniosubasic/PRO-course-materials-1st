78e9d22b1123cb476e3c1ec341beb85768893676
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 009_going_for_gold

---
+++ 009_going_for_gold/Program.cs
@@ -0,0 +1,27 @@
+﻿//going for gold
+
+//defining variables
+int value;
+int gold = 0;
+Console.Write("Enter how much ore you gathered: ");
+int ore = int.Parse(Console.ReadLine()!);
+
+//first rule
+value = Math.Min(ore, 10);
+gold += value * 10;
+ore -= value;
+
+//second rule
+value = Math.Min(ore, 5);
+gold += value * 5;
+ore -= value;
+
+//third rule
+value = Math.Min(ore, 3);
+gold += value * 2;
+ore -= value;
+
+//fourth rule
+gold += ore;
+
+Console.WriteLine("You get " + gold + " gold coins");
