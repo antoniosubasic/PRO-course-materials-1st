c7b0a4b6425afaafff121e81fc6b73916f5a8ef4
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 007_ball_volume_calculator

---
+++ 007_ball_volume_calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+
+Console.Write("Enter the radius of the ball: ");
+int radius = int.Parse(Console.ReadLine()!);
+double ball = 4d / 3 * Math.PI * Math.Pow(radius, 3);
+
+Console.WriteLine("The volume of your ball is: " + ball);
