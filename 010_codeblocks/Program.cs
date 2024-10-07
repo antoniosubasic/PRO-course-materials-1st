61db62378018eb17f1ad52330057f98f97414764
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 010_codeblocks

---
+++ 010_codeblocks/Program.cs
@@ -0,0 +1,11 @@
+﻿{
+    Console.Write("Please enter your name: ");
+    string name = Console.ReadLine()!;
+    Console.WriteLine("Your name is " + name);
+}
+
+{
+    Console.Write("Enter your friend's name: ");
+    string name = Console.ReadLine()!;
+    Console.WriteLine("Your friend's name is " + name);
+}
