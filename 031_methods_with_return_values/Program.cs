00ec90e8a0ec4e67f34f1ad12fdc5c06849446f1
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 031_methods_with_return_values

---
+++ 031_methods_with_return_values/Program.cs
@@ -0,0 +1,29 @@
+﻿// Ask user for width and height of a rectangle
+// Calculate area of rectangle (width * height) IN A SPERATE METHOD
+// Repeat
+
+int width, height;
+
+while (true)
+{
+    Console.Write("\nEnter the width of the rectangle: ");
+    width = LetUserEnterNumber();
+
+    Console.Write("Enter the height of the rectangle: ");
+    height = LetUserEnterNumber();
+
+    int area = CalculateArea();
+    Console.WriteLine($"The are of the rectangle is: {area}");
+}
+
+int CalculateArea()
+{
+    return width * height;
+}
+
+int LetUserEnterNumber()
+{
+    int input;
+    do { input = int.Parse(Console.ReadLine()!); } while (input <= 0);
+    return input;
+}
