c73b435ff9804a5727c9d1f78f4066c2f2608346
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 008_trapez_area

---
+++ 008_trapez_area/Program.cs
@@ -0,0 +1,52 @@
+﻿// bare Program
+{
+    Console.Clear();
+
+    Console.Write("Enter a: ");
+    int a = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter c: ");
+    int c = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter h: ");
+    int h = int.Parse(Console.ReadLine()!);
+
+    double area = (a + c) * h / 2d;
+
+    Console.WriteLine("The area is: " + area);
+
+    Console.WriteLine("\nPress any key to continue...");
+    Console.ReadKey();
+}
+
+
+
+// ascii art
+{
+    Console.Clear();
+
+    string trapez = @"
+            c
+     ______________
+    /|             \
+   / |              \
+  /  | h             \
+ /   |                \
+/____|_________________\
+            a
+";
+    Console.WriteLine(trapez);
+
+    Console.Write("\n\nEnter a: ");
+    int a = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter c: ");
+    int c = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Enter h: ");
+    int h = int.Parse(Console.ReadLine()!);
+
+    double area = (a + c) * h / 2d;
+
+    Console.WriteLine("The area is: " + area);
+}
