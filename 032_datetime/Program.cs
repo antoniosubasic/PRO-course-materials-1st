5a5c9c74b8c5954a0f5ae0ba81ffc6d792538e2e
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 032_datetime

---
+++ 032_datetime/Program.cs
@@ -0,0 +1,7 @@
+﻿// Find out which day it is.
+// If it is a weekday (Saturday is a weekday, too), print "I have to work"
+// else (= Sunday) print "Sunday, funday, better than a monday"
+
+Console.WriteLine(IsSunday() ? "Sunday, funday, better than a monday" : "I have to work");
+
+bool IsSunday() => DateTime.Today.DayOfWeek == DayOfWeek.Sunday;
