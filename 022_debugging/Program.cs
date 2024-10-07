daccdbaa7a1c13277ffa9390798d2e6bb9b9ce54
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 022_debugging

---
+++ 022_debugging/Program.cs
@@ -0,0 +1,10 @@
+﻿// Frage Benutzer um eine Zahl
+// Gib alle Zahlen zwischen 0 und der eingegebenen
+// Zahl am Bildschirm aus
+
+Console.Write("Enter a number: ");
+int upperBound = int.Parse(Console.ReadLine()!);
+
+for (int i = 0; i <= upperBound; i++) { Console.WriteLine(i); }
+
+Console.Write("\nGoodbye!");
