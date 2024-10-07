6dc398afca01b39b1d4cea0f0243e3c1744776da
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 003_variables

---
+++ 003_variables/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+Console.Write("Please type in your name: ");
+string name = Console.ReadLine()!;
+
+Console.WriteLine("\nHi " + name + ", how old are you?");
+int age = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("\nHey " + name + ", you are " + age + " years old.");
