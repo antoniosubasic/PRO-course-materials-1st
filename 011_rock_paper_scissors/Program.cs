9dbfe4b05bcae9ac14df7907460fe0d8f3c639aa
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 011_rock_paper_scissors

---
+++ 011_rock_paper_scissors/Program.cs
@@ -0,0 +1,60 @@
+﻿const int ROCK = 1;
+const int PAPER = 2;
+const int SCISSORS = 3;
+const int LIZARD = 4;
+const int SPOCK = 5;
+
+const int DRAW = 0;
+const int PLAYER1_WINS = 1;
+const int PLAYER2_WINS = 2;
+
+const string POSSIBILITIES = ", choose rock (1), paper (2), scissors (3), lizard (4), spock (5): ";
+
+Console.WriteLine("Player 1" + POSSIBILITIES);
+int player1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Player 2" + POSSIBILITIES);
+int player2 = int.Parse(Console.ReadLine()!);
+int winner = DRAW;
+
+if (player1 == ROCK)
+{
+    if (player2 == PAPER) { winner = PLAYER2_WINS; }
+    else if (player2 == SCISSORS) { winner = PLAYER1_WINS; }
+    else if (player2 == LIZARD) { winner = PLAYER1_WINS; }
+    else if (player2 == SPOCK) { winner = PLAYER2_WINS; }
+}
+
+else if (player1 == PAPER)
+{
+    if (player2 == ROCK) { winner = PLAYER1_WINS; }
+    else if (player2 == SCISSORS) { winner = PLAYER2_WINS; }
+    else if (player2 == LIZARD) { winner = PLAYER2_WINS; }
+    else if (player2 == SPOCK) { winner = PLAYER1_WINS; }
+}
+
+else if (player1 == SCISSORS)
+{
+    if (player2 == ROCK) { winner = PLAYER2_WINS; }
+    else if (player2 == PAPER) { winner = PLAYER1_WINS; }
+    else if (player2 == LIZARD) { winner = PLAYER1_WINS; }
+    else if (player2 == SPOCK) { winner = PLAYER2_WINS; }
+}
+
+else if (player1 == LIZARD)
+{
+    if (player2 == ROCK) { winner = PLAYER2_WINS; }
+    else if (player2 == PAPER) { winner = PLAYER1_WINS; }
+    else if (player2 == SCISSORS) { winner = PLAYER2_WINS; }
+    else if (player2 == SPOCK) { winner = PLAYER1_WINS; }
+}
+
+else if (player1 == SPOCK)
+{
+    if (player2 == ROCK) { winner = PLAYER1_WINS; }
+    else if (player2 == PAPER) { winner = PLAYER2_WINS; }
+    else if (player2 == SCISSORS) { winner = PLAYER1_WINS; }
+    else if (player2 == LIZARD) { winner = PLAYER2_WINS; }
+}
+
+if (winner == DRAW) { Console.WriteLine("No winner, repeat the game"); }
+else { Console.WriteLine("Player " + winner + " wins"); }
