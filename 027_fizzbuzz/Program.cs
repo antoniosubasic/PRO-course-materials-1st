f6b0c4fca0bd86e3c137166f2d982943e093a605
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 027_fizzbuzz

---
+++ 027_fizzbuzz/Program.cs
@@ -0,0 +1,24 @@
+﻿for (int i = 1; i < 101; i++)
+{
+    Console.Write(i % 3 == 0 && i % 5 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i.ToString());
+    Console.Write(" ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+int firstNumber = LetUserEnterNumber("first");
+int secondNumber = LetUserEnterNumber("second");
+int thirdNumber = LetUserEnterNumber("third");
+
+for (int i = 1; i <= firstNumber; i++)
+{
+    Console.Write(i % secondNumber == 0 && i % thirdNumber == 0 ? "FizzBuzz" : i % secondNumber == 0 ? "Fizz" : i % thirdNumber == 0 ? "Buzz" : i.ToString());
+    Console.Write(" ");
+}
+
+int LetUserEnterNumber(string nthNumber)
+{
+    Console.Write($"Enter the {nthNumber} number: ");
+    return int.Parse(Console.ReadLine()!);
+}
