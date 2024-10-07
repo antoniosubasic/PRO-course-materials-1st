438d6d89e8ef3b6d80b4722c93f27d1e9f432faa
Antonio Subašić <antonio.subasic.public@gmail.com>
2024-10-07 16:27:31 +0200

    added 004_volume_calculator

---
+++ 004_volume_calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Enter the length: ");
+int length = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the width: ");
+int width = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the height: ");
+int height = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("The volume is: " + length * width * height);
